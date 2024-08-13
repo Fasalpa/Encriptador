49b163369e7e57e854ea90b0877aa2f0e2a7f39e
corpartan <corpartan@gmail.com>
2024-08-13 13:37:07 -0500

    fix replace

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ function encriptador() {
   //remplazar caracteres
 
   //Reemplazo por números
-  let remplazarA = textoPorEncriptar.replace("a", "1");
-  let remplazarE = remplazarA.replace("e", "2");
-  let remplazarI = remplazarE.replace("i", "3");
-  let remplazarO = remplazarI.replace("o", "4");
-  let remplazarU = remplazarO.replace("u", "5");
+  let remplazarA = textoPorEncriptar.replaceAll("a", "1");
+  let remplazarE = remplazarA.replaceAll("e", "2");
+  let remplazarI = remplazarE.replaceAll("i", "3");
+  let remplazarO = remplazarI.replaceAll("o", "4");
+  let remplazarU = remplazarO.replaceAll("u", "5");
 
   //reemplazo por clave
-  let remplazar1 = remplazarU.replace("1", "ai");
-  let remplazar2 = remplazar1.replace("2", "enter");
-  let remplazar3 = remplazar2.replace("3", "imes");
-  let remplazar4 = remplazar3.replace("4", "ober");
-  let remplazar5 = remplazar4.replace("5", "ufat");
+  let remplazar1 = remplazarU.replaceAll("1", "ai");
+  let remplazar2 = remplazar1.replaceAll("2", "enter");
+  let remplazar3 = remplazar2.replaceAll("3", "imes");
+  let remplazar4 = remplazar3.replaceAll("4", "ober");
+  let remplazar5 = remplazar4.replaceAll("5", "ufat");
 
   //llamar panel-encriptado y remplazar el contenido por el texto.
 
@@ -24,18 +24,18 @@ function encriptador() {
 
 function desencriptado() {
   let textoPorDesencriptar = document.getElementById("text-box").value;
-  let remplazarAi = textoPorDesencriptar.replace("ai", "1");
-  let remplazarEnter = remplazarAi.replace("enter", "2");
-  let remplazarImes = remplazarEnter.replace("imes", "3");
-  let remplazarOber = remplazarImes.replace("ober", "4");
-  let remplazarUfat = remplazarOber.replace("ufat", "5");
+  let remplazarAi = textoPorDesencriptar.replaceAll("ai", "1");
+  let remplazarEnter = remplazarAi.replaceAll("enter", "2");
+  let remplazarImes = remplazarEnter.replaceAll("imes", "3");
+  let remplazarOber = remplazarImes.replaceAll("ober", "4");
+  let remplazarUfat = remplazarOber.replaceAll("ufat", "5");
 
   //reemplazo por clave
-  let remplazar1 = remplazarUfat.replace("1", "a");
-  let remplazar2 = remplazar1.replace("2", "e");
-  let remplazar3 = remplazar2.replace("3", "i");
-  let remplazar4 = remplazar3.replace("4", "o");
-  let remplazar5 = remplazar4.replace("5", "u");
+  let remplazar1 = remplazarUfat.replaceAll("1", "a");
+  let remplazar2 = remplazar1.replaceAll("2", "e");
+  let remplazar3 = remplazar2.replaceAll("3", "i");
+  let remplazar4 = remplazar3.replaceAll("4", "o");
+  let remplazar5 = remplazar4.replaceAll("5", "u");
 
   document.getElementById("panel-encriptado").innerText = remplazar5;
 }
